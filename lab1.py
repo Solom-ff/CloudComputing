94adb7929b9f789b8253887dbbecdef982520010
Solom-ff <www.omkar.istalkar@gmail.com>
2025-02-10 13:13:13 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,4 +2,4 @@ print("Cloud Computing Lab Experiment 2")
 print('lab experiment performed')
 
 def file1():
-  print("You are inside File 1\nYou know how to import program from another module\n\n\n\")
+  print("You are inside File 1/nYou know how to import program from another module/n/n/n")
